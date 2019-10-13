ca2f55a7581f5d77425deb21662055855070817e
yifan <yyan522@student.moansh.edu>
2019-10-13 22:25:18 +1100

    Add project files.

---
+++ FIT5032_LAB3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_LAB3.MvcApplication" Language="C#" %>
---
+++ FIT5032_LAB3/Models/FIT5032_LAB3_MODELS.cs
@@ -0,0 +1,28 @@
+namespace FIT5032_LAB3.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class FIT5032_LAB3_MODELS : DbContext
+    {
+        public FIT5032_LAB3_MODELS()
+            : base("name=FIT5032_LAB3_MODELS")
+        {
+        }
+
+        public virtual DbSet<Image> Images { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Image>()
+                .Property(e => e.Path)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Image>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+        }
+    }
+}
